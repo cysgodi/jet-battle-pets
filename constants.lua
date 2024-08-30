b7a08a5c28923c0661ec749dc1a89fb1c9430552
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-08-30 10:57:49 -0400

    add constants for rarity thresholds and default addon settings

--- constants.lua
+++ constants.lua
@@ -2,5 +2,17 @@ local _, ketchum = ...
 
 Ketchum = ketchum -- global namespace
 
--- encounter rate ceiling for a pet to be considered shiny
+-- encounter rate ceiling (inclusive) for a model to be considered uncommon
+ketchum.DEFAULT_UNCOMMON_THRESHOLD = 70
+
+-- encounter rate ceiling (inclusive) for a model to be considered rare
+ketchum.DEFAULT_RARE_THRESHOLD = 40
+
+-- encounter rate ceiling (inclusive) for a model to be considered shiny
 ketchum.DEFAULT_SHINY_THRESHOLD = 10
+
+ketchum.DEFAULT_SETTINGS = {
+  RareThreshold = ketchum.DEFAULT_RARE_THRESHOLD,
+  ShinyThreshold = ketchum.DEFAULT_SHINY_THRESHOLD,
+  UncommonThreshold = ketchum.DEFAULT_UNCOMMON_THRESHOLD
+}
