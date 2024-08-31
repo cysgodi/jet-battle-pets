efa66e555e97666c34851b94f7be4596ca8ea133
Joe Scharnitzke <j.scharnitzke@gmail.com>
2024-08-31 09:11:53 -0400

    add atlas utility function to get texture coordinates

---
+++ utils/atlas.lua
@@ -0,0 +1,13 @@
+local _, ketchum = ...
+
+ketchum.atlas = {}
+
+-- get tex coords from a provided atlas
+function ketchum.atlas:GetTexCoords(atlas)
+  return {
+    atlas.leftTexCoord,
+    atlas.rightTexCoord,
+    atlas.topTexCoord,
+    atlas.bottomTexCoord,
+  }
+end
