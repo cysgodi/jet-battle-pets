689166294c3b060f6f855ce8269f7be80e01552d
cysgodi <j.scharnitzke+wow@gmail.com>
2025-09-11 18:51:17 -0400

    fetch pets from cache

--- init/main.lua
+++ init/main.lua
@@ -7,16 +7,5 @@ JetBattlePets.pets = JetBattlePets.pets or {}
 
 -- fetch info for a pet, using Rematch if possible
 function JetBattlePets.pets.GetPet(petOrSpeciesID)
-  if C_AddOns.IsAddOnLoaded("Rematch") then
-    return Rematch.petInfo:Fetch(petOrSpeciesID)
-  end
-
-  if type(petOrSpeciesID) == "string" then
-    return C_PetJournal.GetPetInfoTableByPetID(petOrSpeciesID)
-  end
-
-  local name = C_PetJournal.GetPetInfoBySpeciesID(petOrSpeciesID)
-  local _, petID = C_PetJournal.FindPetIDByName(name)
-
-  return C_PetJournal.GetPetInfoTableByPetID(petID)
+  return JetBattlePets.cache.pets[petOrSpeciesID]
 end
