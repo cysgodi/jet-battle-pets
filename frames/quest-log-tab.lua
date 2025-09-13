01a188c59067105dc37ef6d932d69e7376882925
cysgodi <j.scharnitzke+wow@gmail.com>
2025-09-13 18:02:01 -0400

    organize XML + load pet quest log entries into a frame pool

--- frames/quest-log-tab.lua
+++ frames/quest-log-tab.lua
@@ -85,6 +85,28 @@ function AddQuestLogTab(name)
   )
 end
 
+local function BattlePets_AddButton(pet, frameIndex)
+  local button = BattlePetScrollFrame.entryFramePool:Acquire();
+
+  button.info = pet
+  button.speciesID = pet.speciesID
+  button.frameIndex = frameIndex
+
+  QuestMapFrame:SetFrameLayoutIndex(button)
+
+  button.Text:SetText(pet.name)
+  button:SetPoint("LEFT", 20, 0)
+  button:Show()
+end
+
+function BattlePetEntry_OnEnter(self)
+  self.HighlightTexture:Show()
+end
+
+function BattlePetEntry_OnLeave(self)
+  self.HighlightTexture:Hide()
+end
+
 ---Override the default `SetChecked` method because we're using a
 ---single atlas with different alpha levels for active vs inactive
 ---states.
@@ -107,18 +129,46 @@ BattlePetsMixin = {}
 function BattlePetsMixin:OnShow()
   self.pets = self.pets or {}
 
-  local mapId = self:GetParent():GetCurrentMapID();
+  local mapId = self:GetParent():GetCurrentMapID()
   local map = JetBattlePets.cache.maps[mapId]
 
   local pets = JetBattlePets.pets.GetPets(map.petIDs)
-  JetBattlePets.array:Each(pets, function(pet)
 
+  JetBattlePets.array:Each(pets, function(pet, index)
+    BattlePets_AddButton(pet, index)
   end)
+
+  self.ScrollFrame.Contents:Layout()
 end
 
 BattlePetScrollFrameMixin = {}
 
 function BattlePetScrollFrameMixin:OnLoad()
+  ScrollFrame_OnLoad(self)
+
+  local onCreateFn = nil
+  local useHighlightManager = true
+  BattlePetScrollFrame.Contents:Init(onCreateFn, useHighlightManager)
+
+  local contentsFrame = self.Contents
+
+  self.entryFramePool = CreateFramePool(
+    "BUTTON",
+    contentsFrame,
+    "QuestLogEntryTemplate",
+    function(framePool, frame)
+      Pool_HideAndClearAnchors(framePool, frame)
+      frame.info = nil
+    end
+  )
+
   self.TitleText:SetText("Battle Pets")
   self.EmptyText:SetText("No Battle Pets")
+end
+
+QuestLogEntryMixin = {}
+
+function QuestLogEntryMixin:OnLoad()
+  self:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+  self.HighlightTexture:Hide()
 end
