d99854b9c5c270c2f623e55de6e7e1064e2b20f6
cysgodi <j.scharnitzke+wow@gmail.com>
2025-09-20 09:17:29 -0400

    don't show quest log tab if the saved var says it's disabled

--- init/frames.lua
+++ init/frames.lua
@@ -5,7 +5,13 @@ local JetBattlePets = _JetBattlePets
 
 JetBattlePets.frames = JetBattlePets.frames or {}
 
+-- TODO: check if non-virtual frames in XML are messing up WarbandWorldQuests
+
 function InitJetBattlePetsFrames()
+  if not JetBattlePets.settings.ENABLE_QUEST_LOG_TAB then
+    return
+  end
+
   AddQuestLogTab("BattlePets")
 
   --re-initialize QuestMapFrame with added tabs and panels
